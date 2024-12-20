e5ba6fa4e45addc8a3d3fc6621d04b0d35a39213
Gopi-490 <miriyalagopi31@gmail.com>
2024-12-20 13:06:37 +0530

    hello world1

--- src/main/java/com/neoteric/HelloWorld.java
+++ src/main/java/com/neoteric/HelloWorld.java
@@ -3,5 +3,6 @@ package com.neoteric;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("hi my name is ----");
     }
 }
